add114a10e9ea7706d9b24a1bdd3c3b23d08c9da
AdwardZheng <panzhengpan@163.com>
2020-05-28 14:51:08 +0800

    客户端连接池添加zookeeper

--- src/main/java/com/netty/rpc/netty/client/NettyChannelPool.java
+++ src/main/java/com/netty/rpc/netty/client/NettyChannelPool.java
@@ -4,6 +4,7 @@ import com.netty.rpc.netty.codec.RpcDecoder;
 import com.netty.rpc.netty.codec.RpcEncoder;
 import com.netty.rpc.netty.codec.RpcRequest;
 import com.netty.rpc.netty.codec.RpcResponse;
+import com.netty.rpc.netty.core.ServiceDiscover;
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.*;
 import io.netty.channel.nio.NioEventLoopGroup;
@@ -11,6 +12,7 @@ import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.util.Attribute;
 
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.ConcurrentHashMap;
@@ -36,21 +38,72 @@ public class NettyChannelPool {
     }
 
 
-    private Channel[] channels;
-    private Object[] locks;
+    private HashMap<String, Channel[]> channelGroups;
+    private HashMap<String, Object[]> lockGroups;
+
+    private String[] addresses;
 
     private static final int MAX_CHANNEL_COUNT = 4;
 
     public NettyChannelPool() {
-        channels = new Channel[MAX_CHANNEL_COUNT];
-        locks = new Object[MAX_CHANNEL_COUNT];
+        ServiceDiscover.getInstance().discover();
 
-        for (int i = 0; i < MAX_CHANNEL_COUNT; i++) {
-            locks[i] = new Object();
+        addresses = new String[ServiceDiscover.serviceList.size()];
+        channelGroups = new HashMap<>();
+        lockGroups = new HashMap<>();
+
+        for (int i = 0; i < addresses.length; i++) {
+            String address = ServiceDiscover.serviceList.get(i);
+            addresses[i] = address;
+            channelGroups.put(address, null);
+
+            Object[] locks = new Object[MAX_CHANNEL_COUNT];
+            for (int j = 0; j < MAX_CHANNEL_COUNT; j++) {
+                locks[j] = new Object();
+            }
+            lockGroups.put(address, locks);
         }
     }
 
+    public void reload() {
+        addresses = new String[ServiceDiscover.serviceList.size()];
+
+        ServiceDiscover.serviceList.toArray(addresses);
+
+        //先剔除无效的服务
+        for (String address : channelGroups.keySet()) {
+            if (!ServiceDiscover.serviceList.contains(address)) {
+                channelGroups.remove(address);
+                lockGroups.remove(address);
+            }
+        }
+
+        //添加新的服务
+        for (String address : ServiceDiscover.serviceList) {
+            if (!channelGroups.containsKey(address)) {
+                channelGroups.put(address, null);
+                Object[] locks = new Object[MAX_CHANNEL_COUNT];
+                for (int i = 0; i < MAX_CHANNEL_COUNT; i++) {
+                    locks[i] = new Object();
+                }
+                lockGroups.put(address, locks);
+            }
+        }
+    }
+
+    private String getAddress() {
+        int random = new Random().nextInt(addresses.length);
+
+        channelGroups.computeIfAbsent(addresses[random], k -> new Channel[MAX_CHANNEL_COUNT]);
+
+        return addresses[random];
+    }
+
     public Channel getChannel() throws InterruptedException {
+        String address = getAddress();
+        Channel[] channels = channelGroups.get(address);
+        Object[] locks = lockGroups.get(address);
+
         int index = new Random().nextInt(MAX_CHANNEL_COUNT);
         Channel channel = channels[index];
 
