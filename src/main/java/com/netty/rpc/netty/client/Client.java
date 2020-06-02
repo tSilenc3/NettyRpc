47d7b093a221c0e1256c6e26c199afd4dac94d17
AdwardZheng <panzhengpan@163.com>
2020-06-02 10:12:01 +0800

    添加测试用例

--- src/main/java/com/netty/rpc/netty/client/Client.java
+++ src/main/java/com/netty/rpc/netty/client/Client.java
@@ -1,16 +1,14 @@
 package com.netty.rpc.netty.client;
 
 import com.netty.rpc.service.HelloService;
-import io.netty.channel.*;
 
 
 public class Client {
 
-    private static Channel channel;
 
     public static void main(String[] args) {
         HelloService helloService = (HelloService) ServiceProxy.getInstance().create(HelloService.class);
-        String result = helloService.sayHello("World");
+        String result = helloService.sayHello("Hello");
         System.out.println(result);
     }
 
