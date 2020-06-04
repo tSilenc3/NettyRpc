75f1ed92d2623bf980e6e251652d765c367e2647
AdwardZheng <panzhengpan@163.com>
2020-06-04 10:00:40 +0800

    删除多余注解

--- src/main/java/com/netty/rpc/service/HelloService.java
+++ src/main/java/com/netty/rpc/service/HelloService.java
@@ -3,8 +3,6 @@ package com.netty.rpc.service;
 import com.netty.rpc.netty.core.RpcService;
 import org.springframework.stereotype.Component;
 
-@RpcService(HelloService.class)
-@Component
 public interface HelloService {
     String sayHello(String name);
 }
